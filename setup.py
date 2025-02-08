14303b8b4b224d80479f1ec8a601e3d179014bfd
apaz-cli <aarpazdera@gmail.com>
2025-02-08 23:12:38 +0000

    Remove __init__

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 setup(
-    name='CPUOptimizer',  # Changed to match your package name
+    name='CPUOptimizer',
     packages=find_packages(),
     ext_modules=[
         CppExtension(
             'CPUOptimizer.bindings',
             ['CPUOptimizer/bindings.cpp'],
-            extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno'],
+            extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno'], # '-fsanitize=address', '-g', '-fno-omit-frame-pointer', '-fsanitize=undefined'
         ),
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    },
+    cmdclass={'build_ext': BuildExtension},
     setup_requires=['torch'],
     install_requires=['torch']
 )
