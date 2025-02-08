243de4cf9b335ee73cca5b7d5a018fd6236f3c3f
apaz-cli <aarpazdera@gmail.com>
2025-02-08 20:57:19 +0000

    test new setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CppExtension
+from setuptools.command.build_ext import build_ext
+import sys
+
+class BuildExt(build_ext):
+    def build_extensions(self):
+        try:
+            # Import PyTorch build tools only when building extensions
+            from torch.utils.cpp_extension import BuildExtension, CppExtension
+            
+            # Create the PyTorch extension
+            extension = CppExtension(
+                'cpu_optimizer_bindings',
+                ['bindings.cpp'],
+                extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno']
+            )
+            
+            # Use PyTorch's BuildExtension
+            torch_build_ext = BuildExtension()
+            torch_build_ext.extensions = [extension]
+            torch_build_ext.build_extensions()
+            
+        except ImportError:
+            sys.exit(
+                "Error: PyTorch is required to build this extension.\n"
+                "Please install PyTorch first: pip install torch"
+            )
 
 setup(
     name='cpu_optimizer_bindings',
-    ext_modules=[
-        CppExtension(
-            'cpu_optimizer_bindings',
-            ['bindings.cpp'],
-            extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno'], #  '-fsanitize=address', '-g', '-fno-omit-frame-pointer', '-fsanitize=undefined'
-        ),
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    version='0.1.0',
+    install_requires=['torch'],
+    cmdclass={'build_ext': BuildExt}
 )
