35da3e8bf8761baeb9caa5b035d52ec749356687
apaz-cli <aarpazdera@gmail.com>
2025-02-18 19:05:08 +0000

    Fix alignment issue.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         CppExtension(
             'CPUOptimizer.bindings',
             ['CPUOptimizer/bindings.cpp'],
-            extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno'], # '-fsanitize=address', '-g', '-fno-omit-frame-pointer', '-fsanitize=undefined'
+            extra_compile_args=['-lm', '-O3', '-march=native', '-fno-math-errno', '-g', '-fno-omit-frame-pointer',]
         ),
     ],
     cmdclass={'build_ext': BuildExtension},
